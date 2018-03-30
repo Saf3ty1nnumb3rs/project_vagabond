f21e5f8cbc45977eef99ea01b6377dea04c13d68
Josh Sample <joshuawsample@gmail.com>
2018-03-30 14:42:19 -0400

    landing page ready

--- client/src/components/LandingView.js
+++ client/src/components/LandingView.js
@@ -4,8 +4,14 @@ import styled from "styled-components";
 const LandingWrap = styled.div`
 background-image: url('https://media.bizj.us/view/img/3972271/skyline-of-atlanta*1024xx1600-900-0-1384.jpg');
 background-size: cover;
+z-index: -10;
+position: fixed;
 height: 100vh;
 width: 100vw;
+.letter-box {
+    margin-top: 50px;
+    color: white;
+}
 
 
 `
@@ -14,7 +20,9 @@ class LandingView extends Component {
     render() {
         return (
             <LandingWrap>
+                <div className="letter-box">
                 <h1>Welcome to Vagabond, James Vagabond</h1>
+                </div>
                 
 
             </LandingWrap>
--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,49 +1,62 @@
 import React, { Component } from "react";
 import styled from "styled-components";
 import { Link } from "react-router-dom";
+import { Button } from "semantic-ui-react";
 
 const NavWrap = styled.div`
   display: flex;
   justify-content: space-between;
   background-color: blueviolet;
+  box-shadow: 1px 4px 8px 1px rgba(0, 0, 0, 0.8);
+  align-items: center;
+  z-index: 1000;
 
   a {
     color: white;
   }
 `;
 
+const LogoWrap = styled.div`
+ margin-left: 10vw;
+`
+
 const NavLinkWrap = styled.div`
   text-align: center;
   display: inline-flex;
   white-space: nowrap;
-  font-size: 28px;
-  margin-right: 5vw;
+  font-size: 20px;
+  margin-right: 8vw;
   color: white;
+  height: 8vw;
+  align-items: center;
+
   a {
+    text-align: center;
     color: white;
     a:visited {
       color: lightgray;
     }
   }
   .inner-nav {
-      margin: 0 12px;
+    margin: 0 12px;
   }
 `;
 class Navbar extends Component {
   render() {
     return (
       <NavWrap>
-        <Link to="/">
-          <h1>Vagabond</h1>
-        </Link>
+        <LogoWrap>
+          <Link to="/">
+            <h1>Vagabond</h1>
+          </Link>
+        </LogoWrap>
         <NavLinkWrap>
-          <div className="inner-nav">
+          <Button color="purple" className="inner-nav">
             <Link to="/">Login</Link>
-          </div>
-          <div className="inner-nav">
+          </Button>
+          <Button color="purple" className="inner-nav">
             <Link to="#">Sign Up</Link>
-          </div>
-          
+          </Button>
         </NavLinkWrap>
       </NavWrap>
     );
