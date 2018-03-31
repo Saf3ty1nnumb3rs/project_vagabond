adadb1dbf35cb94b55dd31a898067d1ebfe5fdb1
arielstitt <arielstitt@gmail.com>
2018-03-31 19:01:48 -0400

    added Header and SingleCityView component

--- client/src/components/showPageView/Header.js
+++ client/src/components/showPageView/Header.js
@@ -23,7 +23,7 @@ class Header extends Component {
     render() {
         return (
             <HeadWrap>
-                <Segment>
+                <Segment raised padded>
                     <CityImgWrapper>
                         <img src={this.props.city.img} alt="" />
                     </CityImgWrapper>
