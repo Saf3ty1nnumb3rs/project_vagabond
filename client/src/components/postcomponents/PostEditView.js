8260e7884a18fe5a81bebc580c9392752f34d9e9
Josh Sample <joshuawsample@gmail.com>
2018-04-01 01:02:23 -0400

    Further broke down post components

---
+++ client/src/components/postcomponents/PostEditView.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class PostEditView extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        );
+    }
+}
+
+export default PostEditView;
