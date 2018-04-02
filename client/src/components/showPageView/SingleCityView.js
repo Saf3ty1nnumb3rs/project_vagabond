9e88f11364fe2ee74f47a14f46aa7216af9b68ab
arielstitt <arielstitt@gmail.com>
2018-04-02 17:41:56 -0400

    sorta fixed image

--- client/src/components/showPageView/SingleCityView.js
+++ client/src/components/showPageView/SingleCityView.js
@@ -10,7 +10,7 @@ const ButtonWrap = styled.div`
   text-align: center;
   margin: 28px auto;
 `;
-
+const HeaderWrapper = styled.div
 
 class SingleCityView extends Component {
   state = {
@@ -22,7 +22,7 @@ class SingleCityView extends Component {
   componentDidMount() {
     this.getSingleCity();
   }
-// this changes the value of the showCreatePost from true to false
+  // this changes the value of the showCreatePost from true to false
   toggleShowAdd = () => {
     this.setState({ showCreatePost: !this.state.showCreatePost });
   };
@@ -53,13 +53,13 @@ class SingleCityView extends Component {
       <div>
         <Header city={this.state.city} />
         <ButtonWrap>
-              <Button animated color="green" onClick={this.toggleShowAdd}>
-                <Button.Content visible>Add New Post</Button.Content>
-                <Button.Content hidden>
-                  <Icon name="comments" />
-                </Button.Content>
-              </Button>
-            </ButtonWrap>
+          <Button animated color="green" onClick={this.toggleShowAdd}>
+            <Button.Content visible>Add New Post</Button.Content>
+            <Button.Content hidden>
+              <Icon name="comments" />
+            </Button.Content>
+          </Button>
+        </ButtonWrap>
         {this.state.showCreatePost ? (
           <Grid.Column computer={6} tablet={8} mobile={16}>
             <CreatePost
@@ -71,16 +71,16 @@ class SingleCityView extends Component {
             />
           </Grid.Column>
         ) : (
-          <Grid.Column computer={6} tablet={8} mobile={16}>
-            <PostList
-              handlePostChange={this.handlePostChange}
-              posts={this.state.posts}
-              cityId={this.props.match.params.id}
-              getSingleCity={this.getSingleCity}
-            />
-            
-          </Grid.Column>
-        )}
+            <Grid.Column computer={6} tablet={8} mobile={16}>
+              <PostList
+                handlePostChange={this.handlePostChange}
+                posts={this.state.posts}
+                cityId={this.props.match.params.id}
+                getSingleCity={this.getSingleCity}
+              />
+
+            </Grid.Column>
+          )}
       </div>
     );
   }
